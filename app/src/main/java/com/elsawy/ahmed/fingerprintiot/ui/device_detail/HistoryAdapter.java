e34ce7d2be16bc529131dcb5fb524cdd7e7e4840
sql_ask_project <ahmedelsawy487@gmail.com>
2020-06-21 16:34:37 +0200

    put ViewHolder in recyclerview adapter

--- app/src/main/java/com/elsawy/ahmed/fingerprintiot/ui/device_detail/HistoryAdapter.java
+++ app/src/main/java/com/elsawy/ahmed/fingerprintiot/ui/device_detail/HistoryAdapter.java
@@ -1,18 +1,25 @@
 package com.elsawy.ahmed.fingerprintiot.ui.device_detail;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.elsawy.ahmed.fingerprintiot.data.Models.HistoryModel;
 import com.elsawy.ahmed.fingerprintiot.R;
+import com.elsawy.ahmed.fingerprintiot.utils.TimeHandle;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class HistoryAdapter extends RecyclerView.Adapter<UserHistoryViewHolder> {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.UserHistoryViewHolder> {
 
     private ArrayList<HistoryModel> historyModelList = new ArrayList<>();
 
@@ -37,5 +44,39 @@ public class HistoryAdapter extends RecyclerView.Adapter<UserHistoryViewHolder> 
         this.historyModelList = historyModelList;
         HistoryAdapter.this.notifyDataSetChanged();
     }
+    class UserHistoryViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.history_username)
+        TextView usernameTV;
+        @BindView(R.id.history_new_state)
+        TextView newStateTV;
+        @BindView(R.id.history_time)
+        TextView timeTV;
+        @BindView(R.id.history_date)
+        TextView dateTV;
+        @BindView(R.id.history_image)
+        ImageView historyChangedWayImage;
+
+        public UserHistoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindToUserHistory(HistoryModel currentUser) {
+            usernameTV.setText(currentUser.getUsername());
+            newStateTV.setText(currentUser.getNewState());
+            timeTV.setText(TimeHandle.getTime(currentUser.getTimestamp()));
+            dateTV.setText(TimeHandle.getDate(currentUser.getTimestamp()));
+            putChangedWayImage(currentUser.getChangedWay());
+        }
+
+        private void putChangedWayImage(String changedWay) {
+            if (changedWay.equals("mobile")) {
+                historyChangedWayImage.setImageResource(R.drawable.ic_phone);
+            } else {
+                historyChangedWayImage.setImageResource(R.drawable.ic_fingerprint);
+            }
+        }
+
+    }
 }
