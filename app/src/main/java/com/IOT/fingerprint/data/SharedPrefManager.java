e34813eec8d339429de5c6a71c63fc333651bf48
sql_ask_project <ahmedelsawy487@gmail.com>
2020-08-02 21:33:55 +0200

    remove static from context

--- app/src/main/java/com/IOT/fingerprint/data/SharedPrefManager.java
+++ app/src/main/java/com/IOT/fingerprint/data/SharedPrefManager.java
@@ -5,14 +5,14 @@ import android.content.SharedPreferences;
 
 public class SharedPrefManager {
     private static SharedPrefManager mInstance;
-    private static Context context;
+    private Context context;
 
     private static final String SHARED_PREF_NAME = "fingerPrintSharedPref";
     private static final String KEY_USERNAME = "username";
     private static final String KEY_USER_EMAIL = "userEmail";
 
     private SharedPrefManager(Context context) {
-        SharedPrefManager.context = context;
+        this.context = context;
     }
 
     public static synchronized SharedPrefManager getInstance(Context context) {
